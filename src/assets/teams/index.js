2a844a5c5d361f0052ff28babe46983846d954a0
Biad <biadboze@gmail.com>
2025-06-01 16:04:42 +0000

    Add new team logos and names to the teamLogos object in index.js for improved representation of additional teams.

--- src/assets/teams/index.js
+++ src/assets/teams/index.js
@@ -7,14 +7,43 @@ export const teamLogos = {
   //   logo: '/path/to/logo.webp',
   //   name: 'Team Name'
   // }
-  uigb: {
+  UIGB: {
     logo: "/teams/flags/uigb.png",
     name: "UIGB",
   },
-  bem: {
+  BEM: {
     logo: "/teams/flags/bem.png",
     name: "BEM",
   },
+  BIU: {
+    logo: "/teams/flags/biu.png",
+    name: "BIU",
+  },
+  HEC: {
+    logo: "/teams/flags/hec.png",
+    name: "HEC",
+  },
+  IUPA: {
+    logo: "/teams/flags/iupa.png",
+    name: "IUPA",
+  },
+  UCAO: {
+    logo: "/teams/flags/ucao.png",
+    name: "UCAO",
+  },
+  IUA: {
+    logo: "/teams/flags/iua.png",
+    name: "IUA",
+  },
+  GSM: {
+    logo: "/teams/flags/gsm.png",
+    name: "GSM",
+  },
+  FUPA: {
+    logo: "/teams/flags/fupa.png",
+    name: "FUPA",
+  },
+
   // Add more teams as needed
 };
 
